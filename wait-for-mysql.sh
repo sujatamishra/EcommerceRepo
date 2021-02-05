0cc25e5e5071c0081cc387d5a8858777261e64ab
sujata <sujata.misra@amzur.com>
2021-02-05 19:55:46 +0530

    Added changes

--- wait-for-mysql.sh
+++ wait-for-mysql.sh
@@ -12,6 +12,9 @@ cmd="$@"
 
 
 python /app/wait-for-mysql.py -h $db_host -P $db_port -p $db_password -d $db_name
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py createsuperuserwithpassword --username admin1 --password admin1 --email admin1@gmail.com
 
 
 exec $cmd
